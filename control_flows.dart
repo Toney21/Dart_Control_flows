4287162607fe75d87d236211e47e1625e4706e86
unknown <you@example.com>
2024-03-13 23:00:45 +0300

    first commit

---
+++ control_flows.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  var num = 8;
+
+  if (num > 10){
+    print("Your number is greater than 10");
+  } else if (num < 10)
+  {
+    print("Your number is less than 10");
+  }else {
+    print("Your number is equal to 10");
+  }
+
+}
